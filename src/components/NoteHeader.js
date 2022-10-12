362f66b2274a37ffac39df463d2626e0df72ea6e
Andi Paris Bachtiar <andiparis02@gmail.com>
2022-10-12 11:01:53 +0700

    Add search event handler

--- src/components/NoteHeader.js
+++ src/components/NoteHeader.js
@@ -1,15 +1,37 @@
 import React from 'react';
-import NoteApp from './NoteApp';
 
 class NoteHeader extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      search: ''
+    }
+
+    this.onSearchNoteChangeEventHandler = this.onSearchNoteChangeEventHandler.bind(this);
+    this.onSearchEventHandler = this. onSearchEventHandler.bind(this);
+  }
+
+  onSearchNoteChangeEventHandler(event) {
+    this.setState(() => {
+      return {
+        search: event.target.value
+      };
+    });
+  }
+
+  onSearchEventHandler(event) {
+    event.preventDefault();
+    this.props.searchNote(this.state);
   }
 
   render() {
     return (
       <div className="note-app__header">
         <h1>Notes</h1>
+        <div className="note-search">
+          <input type="text" placeholder="Masukkan judul catatan yang ingin dicari ..." value={this.state.search} onChange={this.onSearchNoteChangeEventHandler} onKeyUp={this.onSearchEventHandler} />
+        </div>
       </div>
     );
   }
