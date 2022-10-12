e40609f35496f179c4029e10145db1779e0ad329
Andi Paris Bachtiar <andiparis02@gmail.com>
2022-10-12 11:07:42 +0700

    Add data filter & update conditional rendering

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,22 +1,52 @@
 import React from 'react';
 import NoteItem from './NoteItem';
 
-function NoteList({ notes, onDelete }) {
-  return (
-    <div className="notes-list">
-      {
-        notes.length !== 0
-        ? notes.map((note) => (
-          <NoteItem 
-            key={note.id}
-            id={note.id} 
-            onDelete={onDelete}
-            {...note} />
-        ))
-        : <h3 className="notes-list__empty-message">Tidak ada catatan</h3>
-      }
-    </div>
-  );
+function NoteList({ notes, searchKeyword, onDelete }) {
+  const showNotes = notes.filter(note => note.title.toUpperCase().includes(searchKeyword.toUpperCase()));
+
+  if (showNotes.length !== 0 && searchKeyword !== "") {
+    return (
+      <div className="notes-list">
+        {
+          showNotes.map((note) => (
+            <NoteItem 
+              key={note.id}
+              id={note.id} 
+              onDelete={onDelete}
+              {...note} />
+          ))
+        }
+      </div>
+    );
+  } else if (showNotes.length == 0 && searchKeyword !== "") {
+    return (
+      <div className="notes-list">
+        <h3 className="notes-list__empty-message">Catatan tidak ditemukan</h3>
+      </div>
+    );
+  } else {
+    if (notes.length !== 0) {
+      return (
+        <div className="notes-list">
+          {
+            notes.map((note) => (
+              <NoteItem 
+                key={note.id}
+                id={note.id} 
+                onDelete={onDelete}
+                {...note} />
+            ))
+          }
+        </div>
+      );
+    } else {
+      return (
+        <div className="notes-list">
+          <h3 className="notes-list__empty-message">Tidak ada catatan</h3>
+        </div>
+      );
+    }
+  }
 }
 
 export default NoteList;
