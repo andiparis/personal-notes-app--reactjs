18204b649f5672b6aa0c706d9c7dd9cc85af9a5f
Andi Paris Bachtiar <andiparis02@gmail.com>
2022-11-09 16:08:46 +0700

    Remove unused functions

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,48 +1,3 @@
-let notes = [
-  {
-    id: 1,
-    title: "Babel",
-    body: "Babel merupakan tools open-source yang digunakan untuk mengubah sintaks ECMAScript 2015+ menjadi sintaks yang didukung oleh JavaScript engine versi lama. Babel sering dipakai ketika kita menggunakan sintaks terbaru termasuk sintaks JSX.",
-    createdAt: 'Kamis, 14 April 2022',
-    archived: false,
-  },
-  {
-    id: 2,
-    title: "Functional Component",
-    body: "Functional component merupakan React component yang dibuat menggunakan fungsi JavaScript. Agar fungsi JavaScript dapat disebut component ia harus mengembalikan React element dan dipanggil layaknya React component.",
-    createdAt: 'Kamis, 14 April 2022',
-    archived: false,
-  },
-  {
-    id: 3,
-    title: "Modularization",
-    body: "Dalam konteks pemrograman JavaScript, modularization merupakan teknik dalam memecah atau menggunakan kode dalam berkas JavaScript secara terpisah berdasarkan tanggung jawabnya masing-masing.",
-    createdAt: 'Kamis, 14 April 2022',
-    archived: false,
-  },
-  {
-    id: 4,
-    title: "Lifecycle",
-    body: "Dalam konteks React component, lifecycle merupakan kumpulan method yang menjadi siklus hidup mulai dari component dibuat (constructor), dicetak (render), pasca-cetak (componentDidMount), dan sebagainya. ",
-    createdAt: 'Kamis, 14 April 2022',
-    archived: false,
-  },
-  {
-    id: 5,
-    title: "ESM",
-    body: "ESM (ECMAScript Module) merupakan format modularisasi standar JavaScript.",
-    createdAt: 'Kamis, 14 April 2022',
-    archived: false,
-  },
-  {
-    id: 6,
-    title: "Module Bundler",
-    body: "Dalam konteks pemrograman JavaScript, module bundler merupakan tools yang digunakan untuk menggabungkan seluruh modul JavaScript yang digunakan oleh aplikasi menjadi satu berkas.",
-    createdAt: 'Kamis, 14 April 2022',
-    archived: false,
-  },
-];
-
 function showFormattedDate(date) {
   const options = {
     weekday: "long",
@@ -54,24 +9,4 @@ function showFormattedDate(date) {
   return new Date(date).toLocaleDateString("id-ID", options);
 }
 
-function getNotes() {
-  return notes;
-}
-
-function addNote(note) {
-  notes = [
-    ...notes,
-    {
-      id: +new Date(),
-      createdAt: showFormattedDate(new Date()),
-      archived: false,
-      ...note
-    }
-  ]
-}
-
-function deleteNote(id) {
-  notes = notes.filter((note) => note.id !== id);
-}
-
-export { getNotes, showFormattedDate, addNote, deleteNote };
+export { showFormattedDate };
